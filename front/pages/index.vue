85f7946330d0eabc9b47ac40b5e925005cbfe3df
최준용 <ChoiJunYong@choejun-yong-ui-MacBook-Pro.local>
2020-10-19 07:28:10 +0900

    1-2 complate

---
+++ front/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Hello {{name}}</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name:'Nuxt.js',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/pages/signup.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      Signup Page
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name:'Nuxt.js',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
