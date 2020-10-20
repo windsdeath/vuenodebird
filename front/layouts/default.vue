d8857186dea0761abfcc4eba41c5b69c4ddb75af
최준용 <ChoiJunYong@choejun-yong-ui-MacBook-Pro.local>
2020-10-20 13:29:08 +0900

    vuetify

---
+++ front/layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-app>
+        <div>
+            <v-toolbar dark color="green">
+                <v-toolbartitle>
+                    <nuxt-link to="/">Nodebird</nuxt-link>
+                </v-toolbartitle>
+            </v-toolbar>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-text-field>
+                    <v-btn text nuxt to="/profile">
+                        <div>프로필</div>
+                    </v-btn>
+                    <v-btn text nuxt to="/profile">
+                        <div>회원가입</div>
+                    </v-btn>
+                </v-text-field>
+            </v-toolbar-items>
+        </div>
+    </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+a {
+    display: inline-block;
+    text-decoration: none;
+    color: inherit;
+}
+</style>
+
