8b03e50551098f16bcd6c79ab4fe069bed83b4ee
최준용 <ChoiJunYong@choejun-yong-ui-MacBook-Pro.local>
2020-11-06 15:46:16 +0900

    +back

---
+++ back/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('안녕 백엔드 !')
+})
+app.post('/user', (req, res) => {
+    req.body.email;
+    req.body.password;
+    req.body.nickname;
+})
+
+app.listen(3085, () => {
+    console.log(`백엔드 서버가 ${3085}번 포트에서 가동중`)
+})
